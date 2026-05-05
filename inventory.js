7940d17b4daa5839a894e39ece7b3e8feb79705d
dirrtyxmodzz <49265608+dirrtyxmodzz@users.noreply.github.com>
2026-05-04 21:13:02 -0700

    Add files via upload

---
+++ inventory.js
@@ -0,0 +1,51 @@
+const items = [
+
+  // 🪵 Sandpaper (Longblock)
+  { section: "Sandpaper", name: "80 Grit Block", part: "34442", full: "3M Sandpaper 80 Grit", link: "" },
+  { section: "Sandpaper", name: "120 Grit Block", part: "34444", full: "3M Sandpaper 120 Grit", link: "" },
+  { section: "Sandpaper", name: "180 Grit Block", part: "34446", full: "3M Sandpaper 180 Grit", link: "" },
+  { section: "Sandpaper", name: "220 Grit Block", part: "34447", full: "3M Sandpaper 220 Grit", link: "" },
+  { section: "Sandpaper", name: "320 Grit Block", part: "34449", full: "3M Sandpaper 320 Grit", link: "" },
+
+  // 🪵 Sandpaper (Orbital / DA)
+  { section: "Sandpaper", name: "80 Grit DA", part: "31371", full: "3M Cubitron II 80+ 6in Hookit Disc", link: "" },
+  { section: "Sandpaper", name: "120 Grit DA", part: "", full: "3M 120 Grit 6in DA Disc", link: "" },
+  { section: "Sandpaper", name: "180 Grit DA", part: "31374", full: "3M Cubitron II 180+ 6in Hookit Disc", link: "" },
+  { section: "Sandpaper", name: "320 Grit DA", part: "31483", full: "3M Cubitron II 320+ 6in Hookit Disc", link: "" },
+
+  // 🪵 Sandpaper (Mini DA)
+  { section: "Sandpaper", name: "180 Grit Mini DA", part: "31364", full: "3M Cubitron II 3in 180+ Hookit Disc", link: "" },
+  { section: "Sandpaper", name: "320 Grit Mini DA", part: "31463", full: "3M Cubitron II 3in 320+ Hookit Disc", link: "" },
+
+  // 🧪 Fillers / Prep
+  { section: "Fillers / Prep", name: "Glaze", part: "31180", full: "3M Platinum Plus Glaze", link: "" },
+  { section: "Fillers / Prep", name: "Burrito (White)", part: "08369", full: "Burrito White", link: "" },
+  { section: "Fillers / Prep", name: "Adhesive Promoter", part: "06396", full: "3M Adhesive Promoter", link: "" },
+
+  // 🧷 Adhesives / Sealers
+  { section: "Adhesives / Sealers", name: "Bare Metal Seam Sealer", part: "08310", full: "3M Bare-Metal Seam Sealer Beige", link: "" },
+  { section: "Adhesives / Sealers", name: "Panel Bond (Black)", part: "08115", full: "3M Panel Bonding Adhesive", link: "" },
+  { section: "Adhesives / Sealers", name: "Structural Adhesive (Purple)", part: "07333", full: "3M Structural Adhesive", link: "" },
+  { section: "Adhesives / Sealers", name: "4747 (Glue)", part: "04747", full: "3M Seam Sealer Glue", link: "" },
+
+  // 🛠 Consumables
+  { section: "Consumables", name: "File Belt 36+", part: "33443", full: "3M Cubitron II File Belt 36+ (1/2 x 18)", link: "" },
+  { section: "Consumables", name: "Belt Sander 60 Grit", part: "33445", full: "3M Belt Sander 60 Grit", link: "" },
+  { section: "Consumables", name: "Belt Sander 80 Grit", part: "33446", full: "3M Belt Sander 80 Grit", link: "" },
+  { section: "Consumables", name: "Purple Wheels", part: "21552", full: "3M Purple Clean & Strip Wheels", link: "" },
+  { section: "Consumables", name: "Green Corp Wheels", part: "", full: "3M Green Corp Wheels", link: "" },
+  { section: "Consumables", name: "Scotch-Brite Wheels", part: "", full: "3M Scotch-Brite Wheels", link: "" },
+  { section: "Consumables", name: "Cutoff Wheel Thin", part: "34455", full: "Cutoff Wheel Thin", link: "" },
+  { section: "Consumables", name: "Cutoff Wheel Thick (Welds)", part: "", full: "Cutoff Wheel Thick", link: "" },
+  { section: "Consumables", name: "Welding Paper", part: "05916", full: "Welding & Spark Deflection Paper", link: "" },
+  { section: "Consumables", name: "Scotch-Brite Pad", part: "64926", full: "3M Scotch-Brite Pad", link: "" },
+
+  // 🔥 Coatings
+  { section: "Coatings", name: "Weld-Thru Primer", part: "05917", full: "3M Weld-Thru Coating II", link: "" },
+  { section: "Coatings", name: "Cavity Wax", part: "08852", full: "3M Cavity Wax Plus", link: "" },
+  { section: "Coatings", name: "Quick Check", part: "", full: "Quick Check", link: "" },
+
+  // 🧤 PPE
+  { section: "PPE", name: "Mask", part: "", full: "Respirator Mask", link: "" }
+
+];
